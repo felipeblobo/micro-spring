613198c8a1764461a29c6915813569f19d3d7346
felipeblobo <felipeblobo@gmail.com>
2022-02-20 08:21:04 -0300

    ribbon load balance

--- hr-worker/src/main/java/com/lobo/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/lobo/hrworker/resources/WorkerResources.java
@@ -3,6 +3,9 @@ package com.lobo.hrworker.resources;
 import com.lobo.hrworker.entities.Worker;
 import com.lobo.hrworker.repositories.WorkerRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,10 @@ public class WorkerResources {
 
     private WorkerRepository workerRepository;
 
+    private Environment env;
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = workerRepository.findAll();
@@ -25,6 +32,9 @@ public class WorkerResources {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findWorker(@PathVariable final Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker worker = workerRepository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
