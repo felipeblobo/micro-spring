c2ef812677c1318540066f8b695248a60fe6cb71
felipeblobo <felipeblobo@gmail.com>
2022-02-21 09:51:35 -0300

    configuring eureka clients succesfully

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
@@ -1,16 +1,17 @@
-package com.lobo.hrpayroll.client;
+package com.devsuperior.hrpayroll.feignclients;
 
-import com.lobo.hrpayroll.entities.Worker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "hr-worker", path = "/workers")
+import com.devsuperior.hrpayroll.entities.Worker;
+
 @Component
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
-    @GetMapping("/{id}")
-    ResponseEntity<Worker> findWorker(@PathVariable final Long id);
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
 }
--- hr-payroll/src/test/java/com/devsuperior/hrpayroll/HrPayrollApplicationTests.java
+++ hr-payroll/src/test/java/com/devsuperior/hrpayroll/HrPayrollApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lobo.hrpayroll;
+package com.devsuperior.hrpayroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
